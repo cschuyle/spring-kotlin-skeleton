d37d17e45457564e13e353c8b24f0d954b904e95
Carlton Schuyler <carl@dragnon.com>
2024-06-29 11:39:22 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/ktskull/KtskullApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ktskull
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtskullApplication
+
+fun main(args: Array<String>) {
+	runApplication<KtskullApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ktskull/KtskullApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ktskull
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtskullApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
