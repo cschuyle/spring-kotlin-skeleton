7cc39c2a8e5aea105d4ecc6c770a5b5f432e5d78
Carlton Schuyler <carl@dragnon.com>
2024-07-04 10:05:38 +0200

    Add gradle plugin repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 }
 
 dependencies {
-    // Biolerplate
+    // Boilerplate
     implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
